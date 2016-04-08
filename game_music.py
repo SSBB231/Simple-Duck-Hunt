c1eb44db2b0f0e50921f00d2a94343f520ab73fd
joeajjan <josephajjan@hotmail.com>
2016-04-08 10:56:24 -0400

    Update game_music.py

--- game_music.py
+++ game_music.py
@@ -1,11 +1,15 @@
-import pygame
+from pygame import mixer
 
-pygame.mixer.music.load("Duck Hunt Nes Soundtrack.mp3")
-pygame.mixer.music.load("Duck Hunt Sound Effects.mp3")
+music.load("Duck Hunt Nes Soundtrack.mp3")
+music.load("Duck Hunt Sound Effects.mp3")
 
 class game_music:
   
-  def __init__(self, tune):
-    self.tune = tune
+  def __init__(self):
+    self.tune = None
   
-  def play_sound(self):
+  def play_sound(self, tune_name):
+    self.tune = tune_name
+    
+    if tune_name == "gameover":
+      music.set_pos
