47066a1c80fe7e2505f60e592180e0e91318bbac
SSBB231 <jbjdbodden2@gmail.com>
2016-04-09 00:00:37 -0400

    Added Sounds That Play
    Will play shot sound when mouse is clicked.

--- game_music.py
+++ game_music.py
@@ -1,24 +1,21 @@
 import pygame
 
-
-
 class MusicPlayer:
-  
-  def __init__(self):
 	
-	self.sounds = {}
+	def __init__(self):
+		self.sounds = {}
 
-	self.sounds["start"] = pygame.mixer.Sound("start.mp3")
-	self.sounds["get_ready"] = pygame.mixer.Sound("get_ready.mp3")
-	self.sounds["shot"] = pygame.mixer.Sound("shot.mp3")
-	self.sounds["falling"] = pygame.mixer.Sound("falling.mp3")
-	self.sounds["lost_duck"] = pygame.mixer.Sound("lost_duck.mp3")
-	self.sounds["dog_laughing"] = pygame.mixer.Sound("dog_laughing.mp3")
+		self.sounds["start"] = pygame.mixer.Sound("start.wav")
+		#self.sounds["get_ready"] = pygame.mixer.Sound("get_ready.mp3")
+		self.sounds["shot"] = pygame.mixer.Sound("shot.wav")
+		#self.sounds["falling"] = pygame.mixer.Sound("falling.mp3")
+		#self.sounds["lost_duck"] = pygame.mixer.Sound("lost_duck.mp3")
+		#self.sounds["dog_laughing"] = pygame.mixer.Sound("dog_laughing.mp3")
+		
+		self.tune = self.sounds["start"]
+  
+	def play_sound(self, tune_name):
+
+		self.tune = self.sounds[tune_name]
+		pygame.mixer.Sound.play(self.tune)
 	
-    self.tune = self.sounds["start"]
-  
-  def play_sound(self, tune_name):
-  
-    self.tune = self.sounds[tune_name]
-	pygame.mixer.Sound.play(self.tune)
-    
